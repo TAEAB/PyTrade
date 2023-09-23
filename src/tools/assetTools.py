9240724a0bb1a590f0fe5b0e63fdbbba6a46b2d5
FBar000 <barrera.federico@outlook.com>
2023-09-23 12:08:16 -0400

    added type hints / updated documentation / added try/except structure

--- src/tools/assetTools.py
+++ src/tools/assetTools.py
@@ -3,7 +3,7 @@ This initializes the assets.json file.
 '''
 import json
 
-def initializeFunds(funds_usd:float):
+def initializeFunds(funds_usd:float) -> bool:
     '''
     Set all assets to initial values
 
@@ -11,45 +11,51 @@ def initializeFunds(funds_usd:float):
         funds_usd: The initial funds in dollars
     
     Return:
-        (boolean): success or failure
+        success (True) or failure (False)
     '''
     hardSet('funds_usd', 0)
     updateAssets('funds_usd', funds_usd)
 
-def updateAssets(key:str, amt:float):
+def updateAssets(key:str, amt:float) -> bool:
     '''
     Arguments:
         key (string): label of the asset
         amt (float): the change in amount of the asset
         
     Return:
-        (boolean): success or failure
+        success (True) or failure (False)
     '''
-    with open("../py_trading/src/data/assets.json", 'r') as assets_json:
-        assets = json.load(assets_json)
-    # either initialize the json entry or add to its value
-    if key in assets:
-        assets[key] += amt
-    if key not in assets:
-        assets[key] = amt
-    with open('../py_trading/src/data/assets.json', 'w') as assets_json:
-        json.dump(assets, assets_json)
-    return True
+    try: 
+        with open("../py_trading/src/data/assets.json", 'r') as assets_json:
+            assets = json.load(assets_json)
+        # either initialize the json entry or add to its value
+        if key in assets:
+            assets[key] += amt
+        if key not in assets:
+            assets[key] = amt
+        with open('../py_trading/src/data/assets.json', 'w') as assets_json:
+            json.dump(assets, assets_json)
+        return True
+    except: 
+        return False
 
-def trashCollector():
+def trashCollector() -> bool:
     """
     Remove any entries in the stock portion of the JSON with a zero value.
     """
-    with open("../py_trading/src/data/assets.json", 'r') as assets_json:
-        assets = json.load(assets_json)
-    zero_keys = {key for key in assets if assets[key][0] == 0}
-    for key in zero_keys:
-        del assets[key]
-    with open('../py_trading/src/data/assets.json', 'w') as assets_json:
-        json.dump(assets, assets_json)
-    return True
-
-def checkPresence(key:str):
+    try:
+        with open("../py_trading/src/data/assets.json", 'r') as assets_json:
+            assets = json.load(assets_json)
+        zero_keys = {key for key in assets if assets[key][0] == 0}
+        for key in zero_keys:
+            del assets[key]
+        with open('../py_trading/src/data/assets.json', 'w') as assets_json:
+            json.dump(assets, assets_json)
+        return True
+    except:
+        return False
+    
+def checkPresence(key:str) -> bool:
     """
     Check whether the key is in the JSON file.
     """
@@ -57,7 +63,7 @@ def checkPresence(key:str):
         assets = json.load(assets_json)
     return key in assets
 
-def getAmt(key:str):
+def getAmt(key:str) -> float:
     """
     Access amount of an asset in the JSON.
     """
@@ -67,7 +73,7 @@ def getAmt(key:str):
         raise Exception("Target asset not in possesions")
     return assets[key]
 
-def hardSet(key:str, amt:float):
+def hardSet(key:str, amt:float) -> bool:
     '''
     Set the value of an asset in the JSON.
     
@@ -76,14 +82,18 @@ def hardSet(key:str, amt:float):
         amt: new amount of asset
     
     Return:
-        (boolean): success or failure
+        success (True) or failure (False)
     '''
-    with open("../py_trading/src/data/assets.json", 'r') as assets_json:
-        assets = json.load(assets_json)
-    assets[key] = amt
-    with open("../py_trading/src/data/assets.json", 'w') as assets_json:
-        json.dump(assets, assets_json)
-        
+    try: 
+        with open("../py_trading/src/data/assets.json", 'r') as assets_json:
+            assets = json.load(assets_json)
+        assets[key] = amt
+        with open("../py_trading/src/data/assets.json", 'w') as assets_json:
+            json.dump(assets, assets_json)
+        return True
+    except:
+        return False
+    
 if __name__ == "__main__":
     initializeFunds(10)
     updateAssets("aapl", 23)
--- src/tools/credentialTools.py
+++ src/tools/credentialTools.py
@@ -4,22 +4,16 @@ Scripts to edit twelveDataAPI.json
 import json
 
 
-def setToken(token:str):
+def setToken(token:str) -> bool:
     """
     Set the API token.
     """
-    with open("../py_trading/src/data/twelveDataAPI.json", 'r') as f:
-        assets = json.load(f)
-    assets['token'] = token
-    with open("../py_trading/src/data/twelveDataAPI.json", 'w') as f:
-        json.dump(assets, f)
-
-def setToken(token:str):
-    """
-    Set the API token.
-    """
-    with open("../py_trading/src/data/twelveDataAPI.json", 'r') as f:
-        assets = json.load(f)
-    assets['token'] = token
-    with open("../py_trading/src/data/twelveDataAPI.json", 'w') as f:
-        json.dump(assets, f)
+    try: 
+        with open("../py_trading/src/data/twelveDataAPI.json", 'r') as f:
+            assets = json.load(f)
+        assets['token'] = token
+        with open("../py_trading/src/data/twelveDataAPI.json", 'w') as f:
+            json.dump(assets, f)
+        return True
+    except:
+        return False
