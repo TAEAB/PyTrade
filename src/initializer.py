8de7d7a860300a28004152ab5fb0e5ec0b4d8e60
FBar000 <barrera.federico@outlook.com>
2023-09-30 19:51:46 -0400

    deleted initialization of trackers for relative positions of moving averages

--- src/initializer.py
+++ src/initializer.py
@@ -23,12 +23,6 @@ if __name__ == "__main__":
     tracking_list = [x.strip() for x in raw_tracking_list.split(",")]
     for ticker in tracking_list:
         tst.addTicker(ticker)
-    # (Tracking List) Initialize long_is_above_short values
-    for ticker in tracking_list:
-        stock_data = taf.fetch_stock_data(ticker, "1h", 100)
-        long = stock_data['close'].copy().rolling(100).mean()
-        short = stock_data['close'].copy().rolling(50).mean()    
-        long_is_above_short = long > short
     # Initialize Funds
     at.initializeFunds(float(input("Input the initial funds as a float: ")))
     # Reset History
