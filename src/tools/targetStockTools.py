129dc60e2f77af72580c81f83b00b0f308a744d1
FBar000 <barrera.federico@outlook.com>
2023-09-30 19:50:37 -0400

    removed reference to relative position of moving averages

--- src/tools/targetStockTools.py
+++ src/tools/targetStockTools.py
@@ -12,7 +12,6 @@ def addTicker(ticker:str) -> None:
         assets = json.load(f)
     new_target = {
                     "ticker": ticker,
-                    "long_is_above_short": False # False by default--requires initialization
                 }
     assets['list'].append(new_target)
     with open("../py_trading/src/data/targetStock.json", 'w') as f:
@@ -44,13 +43,3 @@ def checkTickers(ticker:str) -> bool:
         assets = json.load(f)
     return ticker in [x["ticker"] for x in assets['list']]
 
-
-# This can be slow for large lists of tracked assets due to linear filtering
-# Unique to moving-average approach / not necessary for other trading heuristics
-def getRelativeAveragePosition(ticker: str) -> bool:
-    """
-    Returns true if 100 day average is greater than 50 day average. False otherwise.
-    """
-    with open("../py_trading/src/data/targetStock.json", 'r') as f:
-        assets = json.load(f)
-    return list(filter(lambda x : x["ticker"] == ticker , assets["list"]))[0]["long_is_above_short"]
