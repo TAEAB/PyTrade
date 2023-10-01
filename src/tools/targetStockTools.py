89aa329cabbfecec8ad17eafa6c8739e56691bfe
FBar000 <barrera.federico@outlook.com>
2023-10-01 12:28:09 -0400

    adapted tools to edit session_data file

--- src/tools/targetStockTools.py
+++ src/tools/targetStockTools.py
@@ -8,38 +8,39 @@ def addTicker(ticker:str) -> None:
     """
     Adds a stock to the list of tracked assets
     """
-    with open("../py_trading/src/data/targetStock.json", 'r') as f:
+    with open("../py_trading/src/data/session_data.json", 'r') as f:
         assets = json.load(f)
     new_target = {
                     "ticker": ticker,
                 }
-    assets['list'].append(new_target)
-    with open("../py_trading/src/data/targetStock.json", 'w') as f:
+    assets['sessions'][-1]['target stock'].append(new_target)
+    with open("../py_trading/src/data/session_data.json", 'w') as f:
         json.dump(assets, f)
 
 def listTickers() -> list:
     """
     Returns a list of the tracked tickers
     """
-    with open("../py_trading/src/data/targetStock.json", 'r') as f:
+    with open("../py_trading/src/data/session_data.json", 'r') as f:
         assets = json.load(f)
-    return [x["ticker"] for x in assets['list']]
+    return [x["ticker"] for x in assets['sessions'][-1]['target stock']]
 
+# Might be irrelevant as new sessions are created 
 def resetTickers() -> None:
     """
     Clears list of tickers
     """
-    with open("../py_trading/src/data/targetStock.json", 'r') as f:
-        assets = json.load(f)
-    assets['list'] = []
-    with open("../py_trading/src/data/targetStock.json", 'w') as f:
-        json.dump(assets, f)
+    with open("../py_trading/src/data/session_data.json", 'r') as f:
+        sessions = json.load(f)
+    sessions['sessions'][-1]["target stock"] = []
+    with open("../py_trading/src/data/session_data.json", 'w') as f:
+        json.dump(sessions, f)
 
 def checkTickers(ticker:str) -> bool:
     """
     Checks whether ticker is tracked in list
     """
-    with open("../py_trading/src/data/targetStock.json", 'r') as f:
-        assets = json.load(f)
-    return ticker in [x["ticker"] for x in assets['list']]
+    with open("../py_trading/src/data/session_data.json", 'r') as f:
+        sessions = json.load(f)
+    return ticker in sessions['sessions'][-1]['target stock']
 
