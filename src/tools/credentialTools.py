b40285cf9851b170cf48c2a26b668393ed0cc02a
FBar000 <barrera.federico@outlook.com>
2023-10-01 12:17:02 -0400

    changed target file for setToken(str)

--- src/tools/credentialTools.py
+++ src/tools/credentialTools.py
@@ -9,10 +9,10 @@ def setToken(token:str) -> bool:
     Set the API token.
     """
     try: 
-        with open("../py_trading/src/data/twelveDataAPI.json", 'r') as f:
+        with open("../py_trading/src/data/session_data.json", 'r') as f:
             assets = json.load(f)
-        assets['token'] = token
-        with open("../py_trading/src/data/twelveDataAPI.json", 'w') as f:
+        assets['sessions'][-1]['API token'] = token
+        with open("../py_trading/src/data/session_data.json", 'w') as f:
             json.dump(assets, f)
         return True
     except:
